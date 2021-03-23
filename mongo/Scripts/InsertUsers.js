e030bb06876d67dd979fbe57eb31873c18efc887
Eliseo Gaston Lopez Ariza <Eliseo.Ariza@Coalfire.com>
2021-03-22 21:54:57 -0300

    latest changes

--- mongo/Scripts/InsertUsers.js
+++ mongo/Scripts/InsertUsers.js
@@ -9,39 +9,39 @@ db = db.getSiblingDB('usuarios');
 db.informacion.insert([
 	{
 	  "_id": "objectid(\"604fa4735a6151ffd89b133d\")",
-	  "nombre": "frank",
-	  "apellido": "pioa",
-	  "equipo": "river"
+	  "Nombre": "frank",
+	  "Appellido": "pioa",
+	  "Equipo": "river"
 	},
 	{
 	  "_id": "objectid(\"604fa4645a6151ffd89b133c\")",
-	  "nombre": "johan",
-	  "apellido": "pipihua",
-	  "equipo": "river"
+	  "Nombre": "johan",
+	  "Appellido": "pipihua",
+	  "Equipo": "river"
 	},
 	{
 	  "_id": "objectid(\"604fa4575a6151ffd89b133b\")",
-	  "nombre": "ever",
-	  "apellido": "chuquipapa",
-	  "equipo": "river"
+	  "Nombre": "ever",
+	  "Appellido": "chuquipapa",
+	  "Equipo": "river"
 	},
 	{
 	  "_id": "objectid(\"604fa4425a6151ffd89b133a\")",
-	  "nombre": "benjaminç",
-	  "apellido": "lopez ariza",
-	  "equipo": "boca"
+	  "Nombre": "benjaminç",
+	  "Appellido": "lopez ariza",
+	  "Equipo": "boca"
 	},
 	{
 	  "_id": "objectid(\"604fa43c5a6151ffd89b1339\")",
-	  "nombre": "julian",
-	  "apellido": "lopez ariza",
-	  "equipo": "boca"
+	  "Nombre": "julian",
+	  "Appellido": "lopez ariza",
+	  "Equipo": "boca"
 	},
 	{
 	  "_id": "objectid(\"604fa3f65a6151ffd89b1338\")",
-	  "nombre": "eliseo",
-	  "apellido": "lopez ariza",
-	  "equipo": "boca"
+	  "Nombre": "eliseo",
+	  "Appellido": "lopez ariza",
+	  "Equipo": "boca"
 	}
 ])
 
---
+++ mongo/Scripts/deleteAllUsers.js
@@ -0,0 +1,8 @@
+print("####################### START SCRIPT.js ##########################");
+
+db = db.getSiblingDB('usuarios');
+
+db.informacion.drop();
+
+
+print("####################### END SCRIPT.js ##########################");
