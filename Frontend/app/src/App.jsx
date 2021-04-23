5e4579a5f76b32ae12c83b963c6185e3638df52a
julian-lope <julilopezariza@gmail.com>
2021-04-23 13:46:25 -0300

    borrando espacios,variables e impots

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid'
-import {BrowserRouter as Router, Switch,Route, Redirect,} from "react-router-dom";
 import Principal from './components/principal';
 
-
-
 export default function App () {
-  return (
-    <Router>
-      <div>
-        <Grid container item xs= {12} >
-          <Principal />
-            
-        </Grid>
-      </div>  
-    </Router>
+  return ( 
+    <div>
+      <Grid container item xs= {12} >
+        <Principal />
+          
+      </Grid>
+    </div>    
   );
 }
--- Frontend/app/src/Components/porcentaje.jsx
+++ Frontend/app/src/Components/porcentaje.jsx
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import Porcentaje1 from './porcentaje1';
 import Porcentaje2 from './porcentaje2';
 
-
-  
-  
 export default function porcentaje () {
       
     return (
         <div>             
             <Grid container item xs={12} >
-                
                 <Grid item xs={6}>
                     <Porcentaje1 />
                 </Grid>
                 <Grid item xs={6} >
                     <Porcentaje2 />
-                </Grid>                      
-                
+                </Grid>                                      
             </Grid>    
         </div>
-    );
-    
+    );  
 }
   
