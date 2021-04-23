c771bacb482298f6436c24b96892bf8ac4c3939b
julian-lope <julilopezariza@gmail.com>
2021-04-23 14:15:39 -0300

    ultimos retoques

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -3,17 +3,11 @@ import Grid from '@material-ui/core/Grid'
 import {BrowserRouter as Router, Switch,Route, Redirect,} from "react-router-dom";
 import Principal from './components/principal';
 
-
-
 export default function App () {
   return (
-    <Router>
-      <div>
         <Grid container item xs= {12} >
           <Principal />
             
         </Grid>
-      </div>  
-    </Router>
   );
 }
--- Frontend/app/src/Components/porcentaje.jsx
+++ Frontend/app/src/Components/porcentaje.jsx
@@ -3,25 +3,16 @@ import Grid from '@material-ui/core/Grid';
 import Porcentaje1 from './porcentaje1';
 import Porcentaje2 from './porcentaje2';
 
-
-  
-  
 export default function porcentaje () {
-      
-    return (
-        <div>             
+    return (            
             <Grid container item xs={12} >
-                
                 <Grid item xs={6}>
                     <Porcentaje1 />
                 </Grid>
                 <Grid item xs={6} >
                     <Porcentaje2 />
-                </Grid>                      
-                
-            </Grid>    
-        </div>
-    );
-    
+                </Grid>                                     
+            </Grid>  
+    );  
 }
   
