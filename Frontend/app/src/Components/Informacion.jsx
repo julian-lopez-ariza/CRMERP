d42108c0ba6b82b5da184b8b361fd41ec6a9b585
julian-lope <julilopezariza@gmail.com>
2021-04-24 00:04:03 -0300

    eliminando etiquetas sin beneficios

--- Frontend/app/src/Components/Informacion.jsx
+++ Frontend/app/src/Components/Informacion.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Porcentaje from './porcentaje';
 import { Divider } from '@material-ui/core';
@@ -13,69 +12,73 @@ const useStyles = makeStyles({
     fontSize: 50,
     margin:'0px 0px 0px 250px ',
   },  
+  margen:{
+    paddingBlock: 2,
+    margin:'50px 0px 50px 0px ',
+  },
 });
 
 export default function Informacion() {
   const classes = useStyles();
-
   return (
     <Grid container item xs={12}>
       <Grid item xs={12}>
         <Card   variant="outlined" >
           <CardContent>
-            <Typography className={classes.title} color="black"  >
+            <h1 className={classes.title} color="black"  >
                 Curriculum vitae
-            </Typography>
-            
-            <Typography variant="h5" component="h2">
+            </h1>          
+            <h3 color="black">
               hola, soy julian!
-            </Typography>
-            
-            <Typography className={classes.pos} color="black">
-            <h1>Conocimientos </h1> 
-            </Typography>
-            
-            <Typography variant="body1" component="p">
+            </h3>             
+            <h1 className={classes.pos} color="black">
+              Conocimientos 
+            </h1>                   
+            <li>
               Estudiante de Lic. Sistemas de la UNLP.
-              <br/>
-              Conocimiento en ReactJS y Angular.
-              <br/>
+            </li>  
+            <li>
+              Conocimiento en ReactJS y Angular.                
+            </li>
+            <li>
               Firebase y REDUX para reactjs.
-              <br/>
-              CSS y UX como maquetador de HTML.
-              <br/>
-              Git como versionado de código, sea en GitHub o TFS.
-              <br/>
-              Scrum o Kanban como metodología de trabajo.	          
-              <br/>
-              <br/>
-              <br/>  
-                            
-              Frontend Web Development ReactJS 
-              <br/>
-              •	Re-factorizar componentes para reutilizarlos en otras partes de la aplicación.
-              <br/>
-              •	Solucionar bugs/inconsistencias de usabilidad en la aplicación.
-              <br/>
-              •	Implementar nuevas funcionalidades del lado de UI en la aplicación.
-              <br/>
-              •	Solucionar bugs en la aplicación.
-              <br/>
-              •	Implementar nuevos componentes de UI en la aplicación.
-              <br/>
-              •	Incorporar material-ui dentro de la aplicación.
-              <br/>
-              •	Utilizamos Scrum como metodología de trabajo con daily, planning y demo.    
-            </ Typography>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <Divider />
+            </li>
+            <li>
+              CSS y UX como maquetador de HTML.               
+            </li>
+            <li>
+              Git como versionado de código, sea en GitHub o TFS.               
+            </li>           
+            <li>
+              Scrum o Kanban como metodología de trabajo.
+            </li>
+            <li>
+              Frontend Web Development ReactJS
+            </li>             
+            <li>
+              Re-factorizar componentes para reutilizarlos en otras partes de la aplicación.
+            </li>              
+            <li>
+              Solucionar bugs/inconsistencias de usabilidad en la aplicación.
+            </li>             
+            <li>
+              Implementar nuevas funcionalidades del lado de UI en la aplicación.
+            </li>
+            <li>
+              Solucionar bugs en la aplicación.
+            </li>             
+            <li>
+              Implementar nuevos componentes de UI en la aplicación.
+            </li>          
+            <li>
+              Incorporar material-ui dentro de la aplicación.
+            </li>              
+            <li>
+              Utilizamos Scrum como metodología de trabajo con daily, planning y demo.   
+            </li>
+            <Divider className={classes.margen}/>
             <Contacto />
-            <Divider />
-            <br/>
-            <br/>            
+            <Divider className={classes.margen} />                      
             <Porcentaje />      
           </CardContent>          
         </Card>
